eb68f74ad2699f7043cfb7fb827a09e5d8c06223
alvesbento/Neto <bento.neto@iesb.edu.br>
2024-10-31 11:22:46 -0300

    teste9

--- src/test.js
+++ src/test.js
@@ -61,11 +61,11 @@ describe('SWAPI Starship Tests', () => {
         expect(response.data.count).toBeGreaterThan(0);
     });
     
-    // test('Get a specific species by ID', async () => {
-    //     const response = await axios.get('https://swapi.dev/api/species/1/');
-    //     expect(response.status).toBe(200);
-    //     expect(response.data.name).toBe('Human');
-    // });
+    test('Get a specific species by ID', async () => {
+        const response = await axios.get('https://swapi.dev/api/species/1/');
+        expect(response.status).toBe(200);
+        expect(response.data.name).toBe('Human');
+    });
     
     // test('Get list of vehicles', async () => {
     //     const response = await axios.get('https://swapi.dev/api/vehicles/');
